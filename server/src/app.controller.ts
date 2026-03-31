aa8e1d8f04d87f5ae35620b520d556128a9a8d2c
harry <harry_emmett@outlook.com>
2026-03-31 18:12:52 +0100

    feat: add server

---
+++ server/src/app.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import type {
+  QuestionsResponse,
+  LikesResponse,
+  PageVisitsResponse,
+} from './schemas/apiSchema';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('questions')
+  getQuestions(): QuestionsResponse {
+    return this.appService.getQuestions();
+  }
+
+  @Get('page-visits')
+  getPageVisits(): PageVisitsResponse {
+    return this.appService.getPageVisits();
+  }
+
+  @Get('likes')
+  getLikes(): LikesResponse {
+    return this.appService.getLikes();
+  }
+}
---
+++ server/src/app.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import fs from 'fs';
+import path from 'path';
+import {
+  QuestionsResponse,
+  questionsResponseSchema,
+  LikesResponse,
+  likesResponseSchema,
+  PageVisitsResponse,
+  pageVisitsResponseSchema,
+} from './schemas/apiSchema';
+
+@Injectable()
+export class AppService {
+  getQuestions(): QuestionsResponse {
+    const fileJson = fs.readFileSync(
+      path.join(process.cwd(), 'src', 'mockApiData', 'mockQuestionsData.json'),
+      'utf-8',
+    );
+
+    return questionsResponseSchema.parse(JSON.parse(fileJson));
+  }
+  getPageVisits(): PageVisitsResponse {
+    const fileJson = fs.readFileSync(
+      path.join(process.cwd(), 'src', 'mockApiData', 'mockPageVisitsData.json'),
+      'utf-8',
+    );
+
+    return pageVisitsResponseSchema.parse(JSON.parse(fileJson));
+  }
+  getLikes(): LikesResponse {
+    const fileJson = fs.readFileSync(
+      path.join(process.cwd(), 'src', 'mockApiData', 'mockLikesData.json'),
+      'utf-8',
+    );
+
+    return likesResponseSchema.parse(JSON.parse(fileJson));
+  }
+}
